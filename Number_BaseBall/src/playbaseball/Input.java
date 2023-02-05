28d687e77fb005a08a16117e36cee09894538af7
N S <kwb020312@naver.com>
2023-02-05 18:55:54 +0900

    사용자 입력 중복제거 기능 수정

--- Number_BaseBall/src/playbaseball/Input.java
+++ Number_BaseBall/src/playbaseball/Input.java
@@ -27,17 +27,26 @@ public class Input {
 		
 		// 중복값 제거
 		List<String> splitTrans = Arrays.asList(getTrans.split(""));
-		splitTrans.stream()
+		splitTrans = splitTrans.stream()
 		.distinct().collect(Collectors.toList());
-		
+		while(splitTrans.size() < 3) {
+			s = new Scanner(System.in);
+			System.out.println("중복되지 않은 숫자를 입력해주세요: ");
+			get = s.nextInt();
+			getTrans = Integer.toString(get);
+			
+			splitTrans = Arrays.asList(getTrans.split(""));
+			splitTrans.stream()
+			.distinct().collect(Collectors.toList());
+		}
 		
 		// 사용자 입력 추가
 		for(String a : splitTrans) {
-
+			
 			myArr.add(Integer.parseInt(a));
 			
 			// 3자리 이상이라면, 즉시 반환
-			if(myArr.size() >= 3) {
+			if(myArr.size() > 3) {
 				String getApply = getTrans.substring(0, 3);
 				System.out.println(getApply + "까지의 숫자만 반영되었습니다.");
 				return myArr;
